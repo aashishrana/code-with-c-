9aef97be7d9f1f9d099385606ec9d40c5304787e
aashishrana <aashishrana71135@gmail.com>
2023-09-16 15:50:15 -0700

    Recursion

---
+++ 03-Recursion/03-printNum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int printNum(int num ) {
+    if(num == 0) {
+        return 0;
+    }
+    cout << num;
+    
+    printNum(num - 1);
+}
+
+int main() {
+	cout<<"GfG!";
+	
+	int result = printNum(5);
+	cout << result << endl;
+	return 0;
+}
