c84f22ed7a6c3cd1eaa03a50c25f5b2d484e411c
aashishrana <aashishrana71135@gmail.com>
2023-10-03 18:28:07 -0700

    New way to string declaration

---
+++ 07-String/strDeclaration.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     string str;
+//     string str1(5, 'n');
+
+//     cout << str1 << endl;
+
+//     return 0;
+// }
+
+int main() {
+    string str;
+    getline(cin, str);
+
+    cout << str;
+
+    return 0;
+}
