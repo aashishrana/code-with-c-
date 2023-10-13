b5bdf0734eb2a59b74f332ba048cb894097de634
aashishrana <aashishrana71135@gmail.com>
2023-10-13 04:56:32 -0700

    Declaration of Binary tree

---
+++ 12-BinaryTree/01-Declaration.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int key;
+    Node *left;
+    Node *right;
+
+    Node(int k) {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+int main() {
+    Node *root = new Node(30);
+    root->left = new Node(20);
+    root->right = new Node(10);
+    root->left->left = new Node(40);
+
+    return 0;
+}
