b8a2e4dfdf91fad374136c95d557eff1851d7b53
Aashish rana <aashishrana71135@gmail.com>
2023-10-25 17:51:35 +0100

    count frequency

---
+++ 02-Arrays/freqOfeleinSortdearr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int countFrequency(int arr[], int size) {
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+
+    }
+}
+
+int main() {
+    int arr[6] = {10,10,10,25,30,30};
+
+}
