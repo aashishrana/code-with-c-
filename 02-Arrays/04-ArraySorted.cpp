2b633b24d9b5a501669810c3a83a99dbff1aa1b0
Aashish rana <aashishrana71135@gmail.com>
2023-11-16 15:16:50 +0000

    checking array is sorted

--- 02-Arrays/04-ArraySorted.cpp
+++ 02-Arrays/04-ArraySorted.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
 
+
+//Nive approach
 bool  isSorted(int arr[], int size) {
     for(int i = 0; i < size; i++) {
         for(int j = i + 1; j < size; j++) {
-            if(arr[i] < arr[j]) {
-                return true;
+            if(arr[i] > arr[j]) {
+                return false;
             }
         }
         
     }
-    return false;
+    return true;
 }
 
 int main() {
     int arr[5] = {1,2,1,4,5};
-
     cout << isSorted(arr, 5);
 
 }
