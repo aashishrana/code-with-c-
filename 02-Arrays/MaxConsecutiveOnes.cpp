c8b442c6c1afee971fee293e92c53bd7dbc51560
Aashish rana <aashishrana71135@gmail.com>
2023-11-29 21:44:03 +0530

    Maximum consecutive ones

---
+++ 02-Arrays/MaxConsecutiveOnes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int maxConsecutiveOnes(bool arr[] , int n) {
+    int res = 0 , curr = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == 0) {
+            curr = 0;
+        }
+
+        else {
+            curr++;
+            res= max(res, curr);
+        }
+    }
+    return res;
+}
+int main() {
+    bool arr[5] = {0,1,1,0,1};
+
+    int ans = maxConsecutiveOnes(arr, 5);
+
+    cout << ans;
+    
+
+}
